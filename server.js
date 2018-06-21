610a3406825151b9bbd8efe716791561eddaf77e
Philippe Luchansky <philuchansky@gmail.com>
2018-06-21 10:58:23 -0700

    changed port to 8000 :)

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const
   express = require('express'),
   app = express(),
-  PORT = 3000
+  PORT = 8000
 
 app.get('/', (req, res) => {
   res.json({ message: "ROOT ROUTE" })
 })
 
-app.listen(3000)
+app.listen(8000)
